d45d60b71bb87dca2b32b71899c6edaeb1692a5d
Sushil Parajuli <sushilparajuli2010@gmail.com>
2021-08-12 15:33:44 +0545

    components added with styles
    - utility method with useRef, useEffect for focus

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
-
+import { AppContainer } from "./styles"
+import { Column } from "./Column"
+import { AddNewItem } from "./AddNewItem"
+import { Card } from "./Card"
 
 export const App = () =>{
-  return null
+  return (
+    <AppContainer>
+     <Column text="todo">
+       <Card text="Generate app scaffhold"/>
+      </Column>
+      <Column text="In Progress">
+       <Card text="Learning Typescript"/>
+      </Column>
+      <Column text="Done">
+       <Card text="Begin to use static typing"/>
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={
+        console.log
+      }/>
+    </AppContainer>
+  )
 }
